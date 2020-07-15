57d079453cd822e2bb343a374a8db19220b68800
lucynguyen <ngu.nguyen@kyanon.digital>
2020-07-15 22:58:42 +0700

    Lecture 35: - Delete BlogPost entities.

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -8,65 +8,111 @@
 
 namespace App\Controller;
 
+use App\Entity\BlogPost;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Serializer;
 
 /**
  * @Route("/blog")
  */
 class PostController extends  AbstractController {
-  private const POSTS = [
-    [
-      'id' => 1,
-      'slug' => 'hello-word',
-      'title' => 'Hello World'
-    ],
-    [
-      'id' => 2,
-      'slug' => 'another-post',
-      'title' => 'This is another post'
-    ],
-    [
-      'id' => 3,
-      'slug' => 'last-example',
-      'title' => 'This is the last example'
-    ],
-  ];
+//  private const POSTS = [
+//    [
+//      'id' => 1,
+//      'slug' => 'hello-word',
+//      'title' => 'Hello World'
+//    ],
+//    [
+//      'id' => 2,
+//      'slug' => 'another-post',
+//      'title' => 'This is another post'
+//    ],
+//    [
+//      'id' => 3,
+//      'slug' => 'last-example',
+//      'title' => 'This is the last example'
+//    ],
+//  ];
 
   /**
-   * @Route("/{page}", name="blog_list", defaults={"page": 1})
+   * @Route("/{page}", name="blog_list", defaults={"page": 1}, requirements={"page":"\d+"})
    */
   public function list($page) {
+    // Fetch data from BlogPost
+    $repository = $this->getDoctrine()->getRepository(BlogPost::class);
+    $items = $repository->findAll();
     return  new JsonResponse([
       'page' => $page,
-      'data' => array_map(function ($item){
-        return $this->generateUrl('blog_by_id', ['id' => $item['id']]);
-      },self::POSTS)
+      'data' => array_map(function (BlogPost $item){
+        return $this->generateUrl('blog_by_slug', ['slug' => $item->getSlug()]);
+      }, $items)
     ]);
   }
 
   /**
-   * @Route("/post/{id}", name="blog_by_id", requirements={"id"="\d+"})
-   * @param $id
+   * @Route("/post/{id}", name="blog_by_id", requirements={"id"="\d+"}, methods={"GET"})
+   * @ParamConverter("post", class="App:BlogPost")
+   *
+   * @param $post
    *
    * @return \Symfony\Component\HttpFoundation\JsonResponse
    */
-  public function post($id) {
-    return new JsonResponse(
-      self::POSTS[array_search($id, array_column(self::POSTS, 'id'))]
+  public function post($post) {
+    return $this->json(
+      #$this->getDoctrine()->getRepository(BlogPost::class)->find($id)
+      $post
     );
   }
 
   /**
    * @Route("/post/{slug}", name="blog_by_slug")
-   * @param $slug
+   * @ParamConverter("post", class="App:BlogPost", options={"mapping": {"slug":"slug"}})
+   * @param \App\Entity\BlogPost $post
    *
    * @return \Symfony\Component\HttpFoundation\JsonResponse
    */
-  public function postBySlug($slug) {
-    return new JsonResponse(
-      self::POSTS[array_search($slug, array_column(self::POSTS, 'slug'))]
+  public function postBySlug($post) {
+    return $this->json(
+      #$this->getDoctrine()->getRepository(BlogPost::class)->findBy(['slug' => $slug])
+      $post
     );
+  }
+
+  /**
+   * @Route("/add", name="blog_add", methods={"POST"})
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
+   *
+   */
+  public function add(Request $request) {
+    /** @var Serializer $serializer */
+    $serializer = $this->get('serializer');
+
+    $blogPost = $serializer->deserialize($request->getContent(), BlogPost::class, 'json');
+
+    $em = $this->getDoctrine()->getManager();
+    $em->persist($blogPost);
+    $em->flush();
+
+    return $this->json($blogPost);
+  }
+    //Lecture 35 Delete entities.
+  /**
+   * @Route("/delete/{id}", name="blog_delete", methods={"DELETE"})
+   * @param \App\Entity\BlogPost $post
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
+   */
+  public  function delete(BlogPost $post) {
+    $em = $this->getDoctrine()->getManager();
+    $em->remove($post);
+    $em->flush();
+    return new JsonResponse(NULL, Response::HTTP_NO_CONTENT);
   }
 }
