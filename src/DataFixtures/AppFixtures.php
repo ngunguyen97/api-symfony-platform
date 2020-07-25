271308cb7ef14a49279a3c9a9d749aafa7c9fd13
lucynguyen <ngu.nguyen@kyanon.digital>
2020-07-25 14:49:54 +0700

    Lecture 43: - composer require --dev fzaninotto/faker. - Generate fake data in fixtures using Faker.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\BlogPost;
+use App\Entity\Comment;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -16,9 +17,13 @@ class AppFixtures extends Fixture
    */
   private $passwordEncoder;
 
+  private $faker;
+
   public function __construct(UserPasswordEncoderInterface $passwordEncoder) {
 
     $this->passwordEncoder = $passwordEncoder;
+    $this->faker = \Faker\Factory::create();
+
   }
 
   public function load(ObjectManager $manager)
@@ -27,37 +32,45 @@ class AppFixtures extends Fixture
         // $manager->persist($product);
         $this->loadUsers($manager);
         $this->loadBlogPosts($manager);
+        $this->loadComments($manager);
 
 
 
 
         // composer require --dev doctrine/doctrine-fixtures-bundle
        // php bin/console doctrine:fixtures:load run this line on terminal to set into BlogPost table.
+      // composer require --dev fzaninotto/faker using to fake data.
     }
 
-    public function  loadBlogPosts(ObjectManager $manager) {
+  public function  loadBlogPosts(ObjectManager $manager) {
       $user = $this->getReference('user_admin');
+       for ($i = 0; $i < 10; $i++) {
+         $blogPost = new BlogPost();
+         $blogPost->setTitle($this->faker->realText(30));
+         $blogPost->setContent($this->faker->realText());
+         $blogPost->setPublished($this->faker->dateTimeThisYear);
+         $blogPost->setAuthor($user);
+         $blogPost->setSlug($this->faker->slug);
+         $this->setReference("blog_post_$i", $blogPost);
+         $manager->persist($blogPost);
+       }
 
-      $blogPost = new BlogPost();
-      $blogPost->setTitle('A fist post 1!');
-      $blogPost->setContent('Post text');
-      $blogPost->setPublished(new \DateTime('2020-07-01 12:00:00'));
-      $blogPost->setAuthor($user);
-      $blogPost->setSlug('a-first-post');
-      $manager->persist($blogPost);
+       $manager->flush();
 
-      $blogPost = new BlogPost();
-      $blogPost->setTitle('A second post 2!');
-      $blogPost->setContent('Post text');
-      $blogPost->setPublished(new \DateTime('2020-07-01 12:00:00'));
-      $blogPost->setAuthor($user);
-      $blogPost->setSlug('a-second-post');
-      $manager->persist($blogPost);
-      $manager->flush();
     }
 
     public function  loadComments(ObjectManager $manager) {
+      for ($i = 0; $i < 10; $i++) {
+        for ($j = 0; $j < rand(1, 10); $j++) {
+          $comment = new Comment();
+          $comment->setContent($this->faker->realText());
+          $comment->setAuthor($this->getReference('user_admin'));
+          $comment->setPublished($this->faker->dateTimeThisYear);
 
+          $manager->persist($comment);
+        }
+      }
+      $manager->flush();
     }
 
     public function loadUsers(ObjectManager $manager) {
